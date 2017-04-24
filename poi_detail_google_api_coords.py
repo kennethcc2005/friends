cc0f1b52f958fe4d695c985eabdbf93953ffec35
Zoesh <zoesh@Zoeshs-MacBook-Pro.local>
2017-04-23 22:25:10 -0500

    updating...

--- poi_detail_google_api_coords.py
+++ poi_detail_google_api_coords.py
@@ -23,34 +23,37 @@ with open('api_key_list.config') as api_key_list_file:
     api_key_list = json.load(api_key_list_file)
 api_key = api_key_list['api_key_list']
 def poi_detail_web():    
-    poi_detail_df = pd.read_csv('poi_detail_df.csv', index_col = 0)
+    poi_detail_df = pd.read_csv('poi_detail_coords_2000.csv', index_col = 0)
     poi_detail_df.geo_content = poi_detail_df.geo_content.astype(str)
-    print 'round1'
+    print 'round2'
     cnt = 0
     i = 0
     for index in poi_detail_df.index:
-        full_address = poi_detail_df.loc[index].address
-        name = poi_detail_df.loc[index]['name']
-        try:
+        if index >= 2000:
+            full_address = poi_detail_df.loc[index].address
+            name = poi_detail_df.loc[index]['name']
+            try:
 
-            result_longlat = find_latlng(full_address, name, api_key[i])
-            while result_longlat == False:
-                i+=1
-                print i, api_key[i]
-                result_longlat = find_latlng(full_address, name, api_i)
-        except:
-            geo_error =1
-            latitude, longitude, geo_content = None, None, None
-        [latitude, longitude, geo_content] = result_longlat 
-        poi_detail_df.set_value(index, 'coord_long', longitude)
-        poi_detail_df.set_value(index, 'coord_lat', latitude)
-        poi_detail_df.set_value(index, 'geo_content', geo_content)
-        
-        if cnt % 10 == 0:
-            print cnt, name
-        if cnt % 1000 == 0:
-            poi_detail_df.to_csv('poi_detail_coords_%s.csv' %(cnt), index_col = None, encoding=('utf-8'))
-        cnt+=1
+                result_longlat = find_latlng(full_address, name, api_key[i])
+                while result_longlat == False:
+                    i+=1
+                    print i, api_key[i]
+                    result_longlat = find_latlng(full_address, name, api_i)
+                [latitude, longitude, geo_content] = result_longlat 
+            except:
+                print 'error', index, name 
+                geo_error =1
+                latitude, longitude, geo_content = None, None, None
+            
+            poi_detail_df.set_value(index, 'coord_long', longitude)
+            poi_detail_df.set_value(index, 'coord_lat', latitude)
+            poi_detail_df.set_value(index, 'geo_content', geo_content)
+            
+            if cnt % 10 == 0:
+                print index, name
+            if cnt % 1000 == 0:
+                poi_detail_df.to_csv('poi_detail_coords_%s.csv' %(cnt), index_col = None, encoding=('utf-8'))
+            cnt+=1
 
     poi_detail_df.to_csv('poi_detail_df_coords.csv',encoding=('utf-8'))
     return poi_detail_df
